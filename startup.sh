d1cf9fd771b5ebbc33d8078d5f7d11cf72beaa89
Min Zhuo <mzhuo@google.com>
2019-09-04 15:48:00 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ sudo apt install python -y
 sudo curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-260.0.0-linux-x86_64.tar.gz
 sudo tar zxvf google-cloud-sdk-260.0.0-linux-x86_64.tar.gz google-cloud-sdk
 sudo ./google-cloud-sdk/install.sh --quiet
-source .bashrc
+source ~/.bashrc
 
 sudo apt install python-pip
 pip install google-cloud-logging
