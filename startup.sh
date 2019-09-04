04a83da4e879ac07cb7756eed2c38c4a2cc93874
Min Zhuo <mzhuo@google.com>
2019-09-04 15:47:20 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 sudo apt install python -y
 sudo curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-260.0.0-linux-x86_64.tar.gz
 sudo tar zxvf google-cloud-sdk-260.0.0-linux-x86_64.tar.gz google-cloud-sdk
-./google-cloud-sdk/install.sh --quiet
+sudo ./google-cloud-sdk/install.sh --quiet
 source .bashrc
 
 sudo apt install python-pip
