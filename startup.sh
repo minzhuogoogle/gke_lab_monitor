bbf95d90eaf399687f16ec7d6cfb57c2914d772b
Min Zhuo <mzhuo@google.com>
2019-09-04 13:12:00 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-gcloud config set account 17851170406-compute@developer.gserviceaccount.com
+
 sudo mkdir -p /home/tmp
 cd /home/tmp
-sudo rm *.py
+sudo rm *.*
 sudo curl -OL https://raw.githubusercontent.com/minzhuogoogle/gke_lab_monitor/master/lab_mon.py
+sudo sync
 sudo chmod +x lab_mon.py
 sudo /home/tmp/lab_mon.py -logger_name cloudaudit.googleapis.com%2Factivity -project csp-gke-231805 -window 7 -user mzhuo@google.com
 sudo /home/tmp/lab_mon.py -logger_name cloudaudit.googleapis.com%2Factivity -project csp-gke-231805 -window 7 -user mzhuo@google.com -gclient
